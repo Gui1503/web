77997606cba1c22145542bbf68aa863230aed4d6
Guilherme Oliveira Sampaio <guilhermesampaio768@gmail.com>
2023-07-26 21:57:13 -0300

    criar script

---
+++ script.js
@@ -0,0 +1,11 @@
+var nome = "Guilherme"
+let idade = 37
+const anoNascimento = 1986
+let idadeEsposa = "37"
+/*
+
+= atribuição
+== comparar se é igual
+=== comparar se é exatamente igual
+*/
+console.log(idade === idadeEsposa);
